a7f440259acccd7c7fc7258273261aaaca902c6b
kendali <bouchaibkendali@gmail.com>
2023-11-19 17:16:46 +0100

    some Change

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,11 @@ const App = () => {
   return (
     <div className="p-5">
       <div className="quote bg-light m-auto p-5 fs-2 rounded">
-        {quote}
+        <q>
+        {
+         quote
+        }
+        </q>
       </div>
       <div className="m-auto text-center p-3">
         <button onClick={handleChange} className="px-5 py-2 bg-main fs-4 text-light rounded">Change Quote</button>
